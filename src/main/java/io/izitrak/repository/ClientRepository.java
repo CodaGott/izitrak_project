4e98a0bb749559b7663c7668c2a9c27c47bb91c8
Uyanwune Chidozie Chukwuebuka <cuyanwune@digicoreltd.com>
2022-01-11 23:37:53 +0100

    added data jpa in repo

--- src/main/java/io/izitrak/repository/ClientRepository.java
+++ src/main/java/io/izitrak/repository/ClientRepository.java
@@ -4,7 +4,10 @@ import io.izitrak.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    List<Client> findByFirstNameOrLastNameOrEmail(String firstName, String lastName, String email);
 
 }
